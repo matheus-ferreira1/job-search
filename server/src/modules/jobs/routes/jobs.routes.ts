785b84f776576f65c89e134347b20e5c79f85f5a
Matheus Ferreira <matheus.fer33@gmail.com>
2024-07-03 23:53:58 -0300

    added fields validation for updating and creating jobs

--- server/src/modules/jobs/routes/jobs.routes.ts
+++ server/src/modules/jobs/routes/jobs.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { celebrate, Joi, Segments } from "celebrate";
+import { JobLocation, JobStatus, JobType } from "@prisma/client";
 
 import { getJobsController } from "../useCases/getJobs/index.js";
 import { getJobByIdController } from "../useCases/getJobById/index.js";
@@ -8,6 +9,10 @@ import { deleteJobController } from "../useCases/deleteJob/index.js";
 import { updateJobController } from "../useCases/updateJob/index.js";
 
 const jobsRoutes = Router();
+
+const JobLocationValues = Object.values(JobLocation);
+const JobTypeValues = Object.values(JobType);
+const JobStatusValues = Object.values(JobStatus);
 
 jobsRoutes.get("/", (req, res) => {
   return getJobsController.handle(req, res);
@@ -25,13 +30,70 @@ jobsRoutes.get(
   }
 );
 
-jobsRoutes.post("/", (req, res) => {
-  return createJobController.handle(req, res);
-});
+jobsRoutes.post(
+  "/",
+  celebrate({
+    [Segments.BODY]: {
+      position: Joi.string().required(),
+      company: Joi.string().required(),
+      jobLocation: Joi.string()
+        .valid(...JobLocationValues)
+        .required()
+        .messages({
+          "any.only": "Invalid job location",
+        }),
+      jobType: Joi.string()
+        .valid(...JobTypeValues)
+        .required()
+        .messages({
+          "any.only": "Invalid job type",
+        }),
+      jobStatus: Joi.string()
+        .valid(...JobStatusValues)
+        .required()
+        .messages({
+          "any.only": "Invalid job status",
+        }),
+    },
+  }),
+  (req, res) => {
+    return createJobController.handle(req, res);
+  }
+);
 
-jobsRoutes.put("/:jobId", (req, res) => {
-  return updateJobController.handle(req, res);
-});
+jobsRoutes.put(
+  "/:jobId",
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      jobId: Joi.string().uuid().required(),
+    }),
+    [Segments.BODY]: {
+      position: Joi.string().required(),
+      company: Joi.string().required(),
+      jobLocation: Joi.string()
+        .valid(...JobLocationValues)
+        .required()
+        .messages({
+          "any.only": "Invalid job location",
+        }),
+      jobType: Joi.string()
+        .valid(...JobTypeValues)
+        .required()
+        .messages({
+          "any.only": "Invalid job type",
+        }),
+      jobStatus: Joi.string()
+        .valid(...JobStatusValues)
+        .required()
+        .messages({
+          "any.only": "Invalid job status",
+        }),
+    },
+  }),
+  (req, res) => {
+    return updateJobController.handle(req, res);
+  }
+);
 
 jobsRoutes.delete(
   "/:jobId",
